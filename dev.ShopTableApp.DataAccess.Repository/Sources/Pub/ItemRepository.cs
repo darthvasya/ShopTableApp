3327d27af0e520205c3b409d953c00a7a9bf19eb
vasyamakarchuk <vasyamakarchuk@outlook.com>
2017-09-14 21:54:18 +0300

    add Repository layer

---
+++ dev.ShopTableApp.DataAccess.Repository/Sources/Pub/ItemRepository.cs
@@ -0,0 +1,26 @@
+﻿using dev.ShopTableApp.DataAccess.EF.Pub;
+using dev.ShopTableApp.DataAccess.EF.Pub.Entity;
+using dev.ShopTableApp.DataAccess.Pub.Contracts;
+using dev.ShopTableApp.DataAccess.Pub.Implementations;
+
+namespace dev.ShopTableApp.DataAccess.Repository.Pub
+{
+    public class ItemRepository : Repository<Item>, IItemRepository
+    {
+        private ShopTableAppDbContext _dataContext;
+
+        public ItemRepository(IDatabaseFactory databaseFactory)
+            : base(databaseFactory)
+        {
+            DatabaseFactory = databaseFactory;
+        }
+
+        protected new IDatabaseFactory DatabaseFactory { get; }
+
+        protected new ShopTableAppDbContext DataContext => _dataContext ?? (_dataContext = DatabaseFactory.Get());
+    }
+
+    public interface IItemRepository : IRepository<Item>
+    {
+    }
+}
---
+++ dev.ShopTableApp.DataAccess.Repository/Sources/Pub/ShopRepository.cs
@@ -0,0 +1,26 @@
+﻿using dev.ShopTableApp.DataAccess.EF.Pub;
+using dev.ShopTableApp.DataAccess.EF.Pub.Entity;
+using dev.ShopTableApp.DataAccess.Pub.Contracts;
+using dev.ShopTableApp.DataAccess.Pub.Implementations;
+
+namespace dev.ShopTableApp.DataAccess.Repository.Pub
+{
+    public class ShopRepository : Repository<Shop>, IShopRepository
+    {
+        private ShopTableAppDbContext _dataContext;
+
+        public ShopRepository(IDatabaseFactory databaseFactory)
+            : base(databaseFactory)
+        {
+            DatabaseFactory = databaseFactory;
+        }
+
+        protected new IDatabaseFactory DatabaseFactory { get; }
+
+        protected new ShopTableAppDbContext DataContext => _dataContext ?? (_dataContext = DatabaseFactory.Get());
+    }
+
+    public interface IShopRepository : IRepository<Shop>
+    {
+    }
+}
