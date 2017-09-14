7921975b1535d55be022bdefd9aac42a4a3da9f7
vasyamakarchuk <vasyamakarchuk@outlook.com>
2017-09-14 22:01:08 +0300

    add bl layer, dto objects

---
+++ dev.ShopTableApp.Common/Sources/Pub/DataTransferObjects/ShopDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dev.ShopTableApp.Common.Pub.DataTransferObjects
+{
+    public class ShopDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public DateTime StartWorkTime { get; set; }
+        public DateTime EndWorkTime { get; set; }
+
+        public virtual ICollection<ItemDto> Items { get; set; } = new HashSet<ItemDto>();
+    }
+}
