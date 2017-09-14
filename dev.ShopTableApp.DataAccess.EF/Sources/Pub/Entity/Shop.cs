ec1add6586559d28c2247d31b47bb06222b15e91
vasyamakarchuk <vasyamakarchuk@outlook.com>
2017-09-14 21:19:08 +0300

    add models

---
+++ dev.ShopTableApp.DataAccess.EF/Sources/Pub/Entity/Shop.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace dev.ShopTableApp.DataAccess.EF.Pub.Entity
+{
+    public class Shop
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        public DateTime StartWorkTime { get; set; }
+        public DateTime EndWorkTime { get; set; }
+
+        public virtual ICollection<Item> Items { get; set; } = new HashSet<Item>();
+    }
+}
---
+++ dev.ShopTableApp.DataAccess.EF/Sources/Pub/ShopTableApp.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using dev.ShopTableApp.DataAccess.EF.Pub.Entity;
+
+namespace dev.ShopTableApp.DataAccess.EF.Pub
+{
+    public class ShopTableAppDbContext : DbContext
+    {
+        public ShopTableAppDbContext()
+            : base("DefaultConnection")
+        {
+            Configuration.LazyLoadingEnabled = false;
+            Database.SetInitializer(new CreateDatabaseIfNotExists<ShopTableAppDbContext>());
+        }
+
+        public DbSet<Shop> Shops { get; set; }
+        public DbSet<Item> Items { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
